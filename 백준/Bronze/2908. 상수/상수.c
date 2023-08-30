9c2ae02f2a2f1c7abac6b56bba08b7160c9e4395
SeoYoung Lee <xeoyounglee@gmail.com>
2023-08-30 16:07:45 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+    a= a/100 + ((a/10)%10)*10 + ((a%100)%10)*100;
+    b= b/100 + ((b/10)%10)*10 + ((b%100)%10)*100;
+    
+    if(a < b)
+        printf("%d", b);
+    else printf("%d", a);
+    
+    
+}
